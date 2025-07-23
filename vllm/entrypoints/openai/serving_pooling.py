0c7dd606509cb51f277a91898542828501a02f6d
Michele Gazzetti <michele.gazzetti1@ibm.com>
2025-07-23 22:53:32 +0000

    Enable Prithvi execution in online mode
    Signed-off-by: Michele Gazzetti <michele.gazzetti1@ibm.com>

--- vllm/entrypoints/openai/serving_pooling.py
+++ vllm/entrypoints/openai/serving_pooling.py
@@ -99,7 +99,10 @@ class OpenAIServingPooling(OpenAIServing):
                 prompt_adapter_request,
             ) = self._maybe_get_adapters(request)
 
-            tokenizer = await self.engine_client.get_tokenizer(lora_request)
+            if self.model_config.skip_tokenizer_init:
+                tokenizer = None
+            else:
+                tokenizer = await self.engine_client.get_tokenizer(lora_request)
 
             if prompt_adapter_request is not None:
                 raise NotImplementedError("Prompt adapter is not supported "
